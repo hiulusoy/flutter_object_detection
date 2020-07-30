b3c490324e1a31a17e805a9bfcf49ff714502f71
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2020-07-30 14:51:15 +0300

    upload project

---
+++ android/app/src/main/kotlin/com/example/my_pretty_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_pretty_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'home.dart';
+
+List<CameraDescription> cameras;
+
+Future<Null> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    print('Error: $e.message');
+  }
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Nesne Takip Uygulaması',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(cameras),
+    );
+  }
+}
